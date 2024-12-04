b3e8d5b3ee1c9ef257d813b052bc9a58f39287a2
NattiiZ <nat87972@gmail.com>
2024-12-04 08:50:55 +0700

    add

---
+++ Lecture_02/var_let_const.js
@@ -0,0 +1,17 @@
+var hello = "Hello";
+
+console.log(hello);
+hello = "Hello World";
+console.log(hello);
+
+if (true)
+{
+    let world = "Hello World";
+    console.log(world);
+}
+// console.log(world);
+
+const fixValue = "Fix Value";
+console.log(fixValue);
+
+// fixValue = "Test assigen value to const";
