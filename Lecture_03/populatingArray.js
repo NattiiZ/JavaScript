303ede5dc6ffe38d290c9efc79c6db43eec13257
NattiiZ <nat87972@gmail.com>
2024-12-25 08:28:53 +0700

    .

---
+++ Lecture_03/populatingArray.js
@@ -0,0 +1,18 @@
+let arr1 = ['A', true, 2];
+
+console.log("\n********* Length and Index *********\n");
+console.log(arr1.length);
+console.log(arr1[3]);
+console.log(arr1[2]);
+
+console.log(arr1[arr1.length-1]);
+
+console.log("\n");
+
+let arr2 = Array(3);
+arr2[2] = "Adding a value!";
+
+console.log(arr2[2]);
+console.log(arr2[arr2.length-1]);
+console.log(arr2[0]);
+console.log(arr2[1]);
