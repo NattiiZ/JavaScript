e58a33a2c080a3c5dc6c5b84d6b33f520cd2d60e
NattiiZ <nat87972@gmail.com>
2024-12-26 05:03:04 +0700

    .

--- !Homework/2-2_Equatation.js
+++ !Homework/2-2_Equatation.js
@@ -17,7 +17,7 @@ function equation(expression) {
     }
 }
 
-console.log(equation("1+1"));  
+console.log(equation("1+1")); 
 console.log(equation("7*4")); 
 console.log(equation("10-5"));  
 console.log(equation("20/4")); 
