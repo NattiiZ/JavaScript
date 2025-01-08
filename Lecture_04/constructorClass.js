f1ce00638ffa60be0db38f7a6f6d7e3ba44f983c
NattiiZ <nat87972@gmail.com>
2025-01-08 08:38:38 +0700

    .

---
+++ Lecture_04/constructorClass.js
@@ -0,0 +1,12 @@
+class Song 
+{
+    constructor(title, artist) 
+    {
+        this.title;
+        this.artist;
+    }
+}
+
+const mySong = new Song("Bohemian Rhapsody", "Queen");
+console.log(mySong.title);
+console.log(mySong.artist);
