07a3f17bef577d989871747cd21617830221eae8
NattiiZ <nat87972@gmail.com>
2025-01-18 11:34:45 +0700

    .

--- !Homework/2-2_Equatation.js
+++ !Homework/2-2_Equatation.js
@@ -1,23 +1,13 @@
-function equation(expression) {
-    const operators = /[+\-*/]/;
-    const parts = expression.split(operators).map(Number);
-    const operator = expression.match(operators)[0];
-
-    switch (operator) {
-        case '+': 
-            return parts[0] + parts[1];
-        case '-': 
-            return parts[0] - parts[1];
-        case '*': 
-            return parts[0] * parts[1];
-        case '/': 
-            return parts[0] / parts[1];
-        default: 
-            return NaN;
+function equation(expression) 
+{
+    try {
+        return new Function('return ' + expression)();
+    } 
+    catch (e) {
+        return NaN;
     }
 }
 
 console.log(equation("1+1")); 
-console.log(equation("7*4")); 
-console.log(equation("10-5"));  
-console.log(equation("20/4")); 
+console.log(equation("7*4-2")); 
+console.log(equation("1+1+1+1+1"));  
